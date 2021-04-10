72819bcd22bb5b4c05ac6be23afeeda37eafaa35
ihoxofixu <ve.gafurov@gmail.com>
2021-04-11 01:12:59 +0300

    well, i don't know why it doesn't work on rosalind, but it must be correct (simple combination of 1D and 1G)

---
+++ BA1H.py
@@ -0,0 +1,21 @@
+def hamming_distance(s1, s2):
+    ans = 0
+    print(s1, s2)
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            ans += 1
+    return ans
+
+
+def PatternCount(text, pattern, dist):
+    count = []
+    for i in range(len(text) - len(pattern) + 1):
+        if hamming_distance(text[i:i+len(pattern)], pattern) <= dist:
+            count.append(i)
+    return count
+
+
+substr = input()
+dna = input()
+d = int(input())
+print(*PatternCount(dna, substr, d))
